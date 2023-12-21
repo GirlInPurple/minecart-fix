7b8190affb538726fda99c89e831c52729dbab45
The Color Blurple <turtlenerd603@gmail.com>
2023-12-21 16:05:33 -0500

    updated how yaw is set, diagonal tracks still broken

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Minecart Fix
-This mod fixes [MC-201](https://bugs.mojang.com/browse/MC-201), a near decade old bug.
+# Minecart View Fix
+This mod fixes [MC-201](https://bugs.mojang.com/browse/MC-201), a near decade old bug.\
+Also adds a command to disable it per-player.
--- src/main/java/xyz/minecartfix/mixin/AbstractMinecartMixin.java
+++ src/main/java/xyz/minecartfix/mixin/AbstractMinecartMixin.java
@@ -3,18 +3,33 @@ package xyz.minecartfix.mixin;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
+import net.minecraft.text.Text;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static xyz.minecartfix.MinecartFixInit.blacklistedPlayers;
 
+import java.text.DecimalFormat;
+
 @Mixin(AbstractMinecartEntity.class)
 public abstract class AbstractMinecartMixin extends Entity {
 
-	public AbstractMinecartMixin(EntityType<?> type, World world) {super(type, world);}
+	public AbstractMinecartMixin(EntityType<?> type, World world, double lastXVelocity, double lastZVelocity, double remaingingTurnRadius) {super(type, world);
+		this.lastXVelocity = lastXVelocity;
+		this.lastZVelocity = lastZVelocity;
+		this.remaingingTurnRadius = remaingingTurnRadius;
+	}
+
+	@Unique
+	private double lastXVelocity;
+	@Unique
+	private double lastZVelocity;
+	@Unique
+	private double remaingingTurnRadius;
 
 	@Inject(at = @At("HEAD"), method = "tick")
 	private void tick(CallbackInfo info) {
@@ -24,7 +39,35 @@ public abstract class AbstractMinecartMixin extends Entity {
 		if (blacklistedPlayers.contains(target)) {return;}
 		if (this.getVelocity().length() == 0) {return;}
 
-		target.setYaw(90 + this.getYaw());
-		target.setPitch(this.getPitch());
+		if (this.getVelocity().x == 0 && this.getVelocity().z > 0) {        // North
+			smoothTurn(target, 0);
+		} else if (this.getVelocity().x == 0 && this.getVelocity().z < 0) { // South
+			smoothTurn(target, 179);
+		} else if (this.getVelocity().x > 0 && this.getVelocity().z == 0) { // East
+			smoothTurn(target, -90);
+		} else if (this.getVelocity().x < 0 && this.getVelocity().z == 0) { // West
+			smoothTurn(target, 90);
+		}
+		/*
+		if (this.lastXVelocity > 0 && this.lastZVelocity > 0) { 		// North East
+			smoothTurn(target,-135);
+		} else if (this.lastXVelocity < 0 && this.lastZVelocity > 0) { 	// North West
+			smoothTurn(target,135);
+		} else if (this.lastXVelocity < 0 && this.lastZVelocity < 0) { 	// South West
+			smoothTurn(target,45);
+		} else if (this.lastXVelocity > 0 && this.lastZVelocity < 0) {  // South East
+			smoothTurn(target, -45);
+		}
+
+		if (this.getVelocity().z != 0) {this.lastXVelocity = this.getVelocity().x;}
+		if (this.getVelocity().x != 0) {this.lastZVelocity = this.getVelocity().z;}
+		*/
+	}
+
+	@Unique
+	private void smoothTurn(Entity target, float targetAngle) {
+		target.setYaw(targetAngle);
+		DecimalFormat decimalFormat = new DecimalFormat("#.##");
+		target.sendMessage(Text.literal("xVel = " + decimalFormat.format(this.getVelocity().x) + " | zVel = " + decimalFormat.format(this.getVelocity().z)+" | this.yaw = "+Math.round(this.getYaw())));
 	}
 }
