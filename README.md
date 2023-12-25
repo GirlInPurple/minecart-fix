8d22ded2ab57e5558536d661a4741a530a25b4cc
The Color Blurple <turtlenerd603@gmail.com>
2023-12-25 02:44:18 -0500

    semi-archive commit

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Minecart View Fix
 This mod fixes [MC-201](https://bugs.mojang.com/browse/MC-201), a near decade old bug.\
 Also adds a command to disable it per-player.
+
+## Semi-Archived
+This project is on hold while I find another way to fix the bug.
+Here are a number of issues I've encountered in this project:
+
+* After crossing diagonal tracks or turns the carts rotates incredibly weirdly, most of the time snapping to the nearest 45-degree angle, usually in the opposite signage.
+  * 180 > -45 > -90 are the rough angles when crossing a T intersection.
+  * Using `target.setYaw(this.getYaw());`, while smooth and quite nice feeling, it tends to be buggy/too snappy due to this issue.
+* `-0` is a valid rotation value, for some reason? I'm guessing its due to some kind of floating point error but im not entirely sure what's going on there.
+  * Checking for specific values is nearly impossible due to this
+  * The `getDirectionVector()` method doesn't always return the correct value, outputting `1.22467991473532E-16` or similar when stopped on a rail, which makes absolutely zero sense.
+
+Due to this, I'm not sure how to continue this project.\
+My original plan was to use the rotation, speed, and `Vec3d` of the minecart to set the target's yaw value.\
+Considering 2 out of 3 of those are broken/not always functional, im not sure how to approach this bug anymore.\
+If someone has an idea of how to fix this issue, feel free to fork this and make a PR.\
+In the meantime, I'll be trying to find another way to do this. No ETA for this, and possibly never.
--- src/main/java/xyz/minecartfix/mixin/AbstractMinecartMixin.java
+++ src/main/java/xyz/minecartfix/mixin/AbstractMinecartMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -11,63 +12,66 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static xyz.minecartfix.MinecartFixInit.blacklistedPlayers;
-
-import java.text.DecimalFormat;
-
 @Mixin(AbstractMinecartEntity.class)
 public abstract class AbstractMinecartMixin extends Entity {
 
-	public AbstractMinecartMixin(EntityType<?> type, World world, double lastXVelocity, double lastZVelocity, double remaingingTurnRadius) {super(type, world);
-		this.lastXVelocity = lastXVelocity;
-		this.lastZVelocity = lastZVelocity;
-		this.remaingingTurnRadius = remaingingTurnRadius;
+	public AbstractMinecartMixin(EntityType<?> type, World world) {
+		super(type, world);
 	}
 
+	/**
+	 * Gets the direction the entity is going.
+	 * In this case, it's used to find what direction the minecart is going.
+	 *
+	 * @return a {@link net.minecraft.util.math.Vec3d} of the entity its being used on.
+	 * */
 	@Unique
-	private double lastXVelocity;
-	@Unique
-	private double lastZVelocity;
-	@Unique
-	private double remaingingTurnRadius;
+	public Vec3d getDirectionVector() {
+		float yaw = this.getYaw();
+		float pitch = this.getPitch();
+		double x = -Math.sin(Math.toRadians(yaw)) * Math.cos(Math.toRadians(pitch));
+		double y = -Math.sin(Math.toRadians(pitch));
+		double z = Math.cos(Math.toRadians(yaw)) * Math.cos(Math.toRadians(pitch));
+		return new Vec3d(x, y, z);
+	}
 
+	/**
+	 * The main mixin.
+	 * Checks what direction the minecart is going and sets the yaw of the target to that direction
+	 * It currently does not work.
+	 * */
 	@Inject(at = @At("HEAD"), method = "tick")
 	private void tick(CallbackInfo info) {
 		Entity target = this.getFirstPassenger();
-
 		if (target == null) {return;}
-		if (blacklistedPlayers.contains(target)) {return;}
-		if (this.getVelocity().length() == 0) {return;}
-
-		if (this.getVelocity().x == 0 && this.getVelocity().z > 0) {        // North
-			smoothTurn(target, 0);
-		} else if (this.getVelocity().x == 0 && this.getVelocity().z < 0) { // South
-			smoothTurn(target, 179);
-		} else if (this.getVelocity().x > 0 && this.getVelocity().z == 0) { // East
-			smoothTurn(target, -90);
-		} else if (this.getVelocity().x < 0 && this.getVelocity().z == 0) { // West
-			smoothTurn(target, 90);
+		Vec3d vector = getDirectionVector();
+		// I hate floating point with a burning passion.
+		double approximatelyZero = 1.2246468;
+		if (Math.abs(vector.x) >= approximatelyZero && Math.abs(vector.z) == approximatelyZero) { // east
+			target.sendMessage(Text.literal("east | "+vector+" | approx | "+Math.abs(vector.x) + ", "+Math.abs(vector.z)));
+		} else if (Math.abs(vector.x) >= approximatelyZero && Math.abs(vector.z) >= approximatelyZero) { // north-east
+			target.sendMessage(Text.literal("north-east | " + vector+" | approx | "+Math.abs(vector.x) + ", "+Math.abs(vector.z)));
+		} else if (Math.abs(vector.x) == approximatelyZero && Math.abs(vector.z) >= approximatelyZero) { // north
+			target.sendMessage(Text.literal("north | " + vector+" | approx | "+Math.abs(vector.x) + ", "+Math.abs(vector.z)));
+		} else if (Math.abs(vector.x) <= approximatelyZero && Math.abs(vector.z) >= approximatelyZero) { // north-west
+			target.sendMessage(Text.literal("north-west | " + vector+" | approx | "+Math.abs(vector.x) + ", "+Math.abs(vector.z)));
+		} else if (Math.abs(vector.x) <= approximatelyZero && Math.abs(vector.z) == approximatelyZero) { // west
+			target.sendMessage(Text.literal("west | " + vector+" | approx | "+Math.abs(vector.x) + ", "+Math.abs(vector.z)));
+		} else if (Math.abs(vector.x) <= approximatelyZero && Math.abs(vector.z) <= approximatelyZero) { // south-west
+			target.sendMessage(Text.literal("south-west | " + vector+" | approx | "+Math.abs(vector.x) + ", "+Math.abs(vector.z)));
+		} else if (Math.abs(vector.x) == approximatelyZero && Math.abs(vector.z) <= approximatelyZero) { // south
+			target.sendMessage(Text.literal("south | " + vector+" | approx | "+Math.abs(vector.x) + ", "+Math.abs(vector.z)));
+		} else if (Math.abs(vector.x) >= approximatelyZero && Math.abs(vector.z) <= approximatelyZero) { // south-east
+			target.sendMessage(Text.literal("south-east | " + vector+" | approx | "+Math.abs(vector.x) + ", "+Math.abs(vector.z)));
 		}
-		/*
-		if (this.lastXVelocity > 0 && this.lastZVelocity > 0) { 		// North East
-			smoothTurn(target,-135);
-		} else if (this.lastXVelocity < 0 && this.lastZVelocity > 0) { 	// North West
-			smoothTurn(target,135);
-		} else if (this.lastXVelocity < 0 && this.lastZVelocity < 0) { 	// South West
-			smoothTurn(target,45);
-		} else if (this.lastXVelocity > 0 && this.lastZVelocity < 0) {  // South East
-			smoothTurn(target, -45);
-		}
-
-		if (this.getVelocity().z != 0) {this.lastXVelocity = this.getVelocity().x;}
-		if (this.getVelocity().x != 0) {this.lastZVelocity = this.getVelocity().z;}
-		*/
 	}
 
+	/**
+	 * An example function to set the yaw of the target.
+	 * @param target The entity in the minecart.
+	 * */
 	@Unique
-	private void smoothTurn(Entity target, float targetAngle) {
-		target.setYaw(targetAngle);
-		DecimalFormat decimalFormat = new DecimalFormat("#.##");
-		target.sendMessage(Text.literal("xVel = " + decimalFormat.format(this.getVelocity().x) + " | zVel = " + decimalFormat.format(this.getVelocity().z)+" | this.yaw = "+Math.round(this.getYaw())));
+	private void tempSetYaw(Entity target) {
+		target.setYaw(this.getYaw());
 	}
 }
